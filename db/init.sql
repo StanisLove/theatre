1539d415f71c3bc26a91c461c0cd757c0ac8e268
Stanislav Kravchenko <stankra@yandex.ru>
2020-08-28 19:40:56 +0300

    Add init.sql

---
+++ db/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE theatre_development;
+GRANT ALL PRIVILEGES ON DATABASE theatre_development TO username;
+
+CREATE DATABASE theatre_test;
+GRANT ALL PRIVILEGES ON DATABASE theatre_test TO username;
